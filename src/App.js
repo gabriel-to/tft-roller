161c08cce7722685fa0690595b7677c90ecdf4cc
Gabriel <Gabriel@DESKTOP-R33DKU2>
2021-09-03 15:17:54 -0400

    beta v0.1

--- src/App.js
+++ src/App.js
@@ -16,25 +16,29 @@ function App() {
       <Navbar/>
       <form>
         <p>
-          <label for="level">Level: </label>
+          <label for="level">Level</label>
           <input type="number" id="level"></input>
         </p>
         <p>
-          <label for="gold">Gold: </label>
+          <label for="gold">Gold</label>
           <input type="number" id="gold"></input>
         </p>
         <p>
-          <label for="cost">Unit cost:</label>
+          <label for="cost">Unit cost</label>
           <input type="number" id="cost"></input>
         </p>
         <p>
-          <label for="taken">Units taken:</label>
+          <label for="taken">Units taken</label>
           <input type="number" id="taken"></input>
         </p>
         <button onClick={activateGamba}>gamba</button>
         <p>avg unit on rolldown: {gamba}</p>
       </form>
-      
+      <div className="footer">
+        <p>tft-roller isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. 
+          League of Legends™ and Riot Games are trademarks or registered trademarks of Riot Games, Inc.</p>
+            Contact: togabriel@protonmail.com
+      </div>
     </div>
     
   );
